af139e262224ccc67dba8f0ffa9cc7a3da3e8978
p47d4 <107918010+p47d4@users.noreply.github.com>
2025-10-17 04:32:28 +0100

    Fix import path for Packages component

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,7 @@ import { Header } from '@/components/Header';
 import { Hero } from '@/components/Hero';
 import { Services } from '@/components/Services';
 import { PackagesSection } from '@/components/PackagesSection';
-import { Packages } from '@/Packages';
+import { Packages } from 'Packages';
 import { Footer } from '@/components/Footer';
 
 const Index = () => {
